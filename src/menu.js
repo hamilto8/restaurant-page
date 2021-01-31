2196b00e136abf09cb0d63ded2846815463cd034
= <michael.ts.hamilton@gmail.com>
2021-01-31 10:20:37 -0600

    Added images to the menu page. Tweaked some styling elements.

--- src/menu.js
+++ src/menu.js
@@ -3,10 +3,25 @@ const displayMenu = () => {
     <div class="menu">
         <div class="foods">
             <h4>Foods:</h4>
-            <p>Salad: $1.50</p>
-            <p>Soup: $7.59</p>
-            <p>Pizza: $8.99</p>
-            <p>Pasta: $9.99</p>
+            <div class="menu-item">
+                <img src="/images/salad.png"/>
+                <p>Salad: $1.50</p>
+            </div>
+            <div class="menu-item">
+                <img src="/images/soup.png"/>
+                <p>Soup: $7.59</p>
+            </div>
+            <div class="menu-item">
+                <img src="/images/pizza-sm.png"/>
+                <p>Pizza: $8.99</p>
+            </div>
+            <div class="menu-item">
+                <img src="/images/pasta.png"/>
+                <p>Pasta: $9.99</p>
+            </div>
+        </div>
+        <div class="background-div">
+            <img class="hero-image" src="/images/restaurant.jpg" />
         </div>
     </div>
     `;
